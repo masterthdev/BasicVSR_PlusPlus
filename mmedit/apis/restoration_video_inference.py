a5553d2a746d0473daaa0118f3213a4b0e935511
masterthdev <64798939+masterthdev@users.noreply.github.com>
2023-03-07 21:52:53 +0300

    videoreader memory clean test

--- mmedit/apis/restoration_video_inference.py
+++ mmedit/apis/restoration_video_inference.py
@@ -48,18 +48,20 @@ def restoration_video_inference(model,
     import math
     video_size = len(video_reader)
     batch_count = math.ceil(video_size/max_seq_len)
-    
+    video_reader = null
     for batch in range(batch_count):
         batch_begin = batch * max_seq_len
         batch_end = batch_begin + max_seq_len
         # load the images
         data = dict(lq=[], lq_path=None, key=img_dir)
         framno = 0
+        video_reader = mmcv.VideoReader(img_dir)
         for frame in video_reader:
             if framno >= batch_begin and framno < batch_end:
                 data['lq'].append(np.flip(frame, axis=2))
                 print("vidframe " + str(framno))
             framno += 1
+        video_reader = null
 
         # remove the data loading pipeline
         tmp_pipeline = []
